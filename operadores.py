b1cffa2c415b69cba3c5526e3350b8af06720738
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-03-07 16:07:42 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,23 @@
+X =int(input("digite um numero "))
+Y=int(input("Digite o segundo numero "))
+
+#criando uma operação de soma e armazenando na variavel soma
+
+soma = X+Y
+print(f"Sua soma de {X}+{Y} deu {soma}")
+
+#CRIANDO SUBTRAÇÃO NA VARIAVEL SUB
+sub= X - Y
+print(f"Sua subtração de {X}-{Y} deu {sub}")
+
+#CRIANDO UMA MULTIPLICAÇÃO NA VARIAVEL MULTI
+multi= X * Y
+print(f"Sua multiplicação de {X}x{Y} deu {multi}")
+
+#CRIANDO UMA DIVISAO E ARMAZENANDO NA VARIAVEL DIV
+div = X / Y
+print(f"Sua divisão de {X}x{Y} deu {div}")
+
+#CRIANDO UMA OPERAÇÃO DE RESTO NA VARIAVEL RESTO
+resto = X % Y
+print(f"O resto da divisão de {X}/{Y} deu {resto}")
