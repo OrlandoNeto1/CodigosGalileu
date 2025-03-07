977510a1381df82aed13e90d2525f11599b7ea13
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-03-07 16:31:57 -0300

    Add files via upload

---
+++ comparadores.py
@@ -0,0 +1,8 @@
+comparador1 = input("Digite o primeiro numero: ")
+comparador2 = input("digite o segundo numero: ")
+comparador = comparador1>comparador2
+print(comparador)
+print (f"Seu resultado de {comparador2}<{comparador1} esta {comparador}")
+comparador = comparador1<comparador2
+print(comparador)
+print (f"Seu resultado de {comparador2}>{comparador1} esta {comparador}")
