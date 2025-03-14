59e87bf96aeb66795bd228299528edbe58508896
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-03-14 15:45:47 -0300

    Add files via upload
    V2

--- condicionais.py
+++ condicionais.py
@@ -1,9 +1,15 @@
 idade_orlando = int(input("Digite sua idade:  "))
+carteira = input("Você tem cateira de motorista?: (Responda 'S' para sim e 'N' para não) ")
+
 
 #VOU DETALHAR UMA CONDICIONAL PARA VERIFICAR SE A IDADE É MAIOR DE 18 ANOS
 
-if idade_orlando > 18: 
+if idade_orlando >= 18 and carteira == 'S':
      print("Você pode dirigir!")
+
+elif carteira != 'S' or 'N':
+    print("Não entendi! Digite 'S' ou 'N'!:  ")
+
 
 else:
      print("Você não pode dirigir")
