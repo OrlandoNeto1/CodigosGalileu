33bd34439e18565ef157cf5bad601fd691051e6d
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-03-14 15:30:25 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,9 @@
+idade_orlando = int(input("Digite sua idade:  "))
+
+#VOU DETALHAR UMA CONDICIONAL PARA VERIFICAR SE A IDADE É MAIOR DE 18 ANOS
+
+if idade_orlando > 18: 
+     print("Você pode dirigir!")
+
+else:
+     print("Você não pode dirigir")
