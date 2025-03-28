7a3d8a232469c065a6146a0020857d391a26a8da
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-03-28 15:29:06 -0300

    Add files via upload

--- while.py
+++ while.py
@@ -1,7 +1,7 @@
 resposta = input("Digite 'S' para sim e 'N' para não : ")
-resposta_tentativa = str(input("Digite 'S' para sim e 'N' para não : "))
+
 #ENQUANTO A RESPOSTA FOR DIFERENTE DE s OU n QUERO QUE REPITA O INPU
-while resposta != 'S' or resposta !='N':
+while resposta not in ('S', 'N'):
     print("Você errou! Digite novamente!")
     resposta = input("Digite 'S' para sim e 'N' para não : ")
      
