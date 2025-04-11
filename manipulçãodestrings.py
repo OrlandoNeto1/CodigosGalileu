7057950dd0f6957f15700832c8d0f4174c6a0f11
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-04-11 16:35:55 -0300

    Add files via upload

--- manipulçãodestrings.py
+++ manipulçãodestrings.py
@@ -18,8 +18,24 @@ conta_bancaria=2000000
 print(f"{conta_bancaria:,}") 
 
 #frase base para dominar varios metodos de manu=ipulação de string
-frase = "Eu hackiei o Bing"
 
-#METODO CAPITALIZE()DA BIBLIOTECA PADRÃO DE STRINGS DO PYTHON
+frase = input("Escolha uma frase:  ")
+frutas = "Maçã, Morango, Uva"
+lista_palavras = ["Álvaro","mora", "no", "parque" ,"do", "lago"]
+frase_zuada = "            OrlAnDo TEM Um CAChoRro CHamaDo TObias, ElE É uM SHITZU"
+#METODO "X()"DA BIBLIOTECA PADRÃO DE STRINGS DO PYTHON
+
 print(frase.capitalize())#coloca a primeira letra da frase em maiusculo
 print (frase.title())# coloca a primeira letra de cada palavra em maiusculo
+print(frase.upper())# coloca toda frase em maiuscula
+print(len(frase))#conta a qtd de caracteres de uma frase
+print(frase.lower())#coloca toda frase em letra minuscula
+print(frase.center(50,'_'))#centraliza uma palavra repitindo os caracteres informados, SOMENTE COM UMA PALAVRA
+print(frase.find("ontem!"))#retorna a posição da primeira letra encontrada
+print(frase.replace('alvaro', 'orlando'))#substitui uma palavra por outra especificada
+print(frase.startswith("a"))#verifica se a primeira letra da frase é a letra especificada
+print(frase.endswith("é"))#verifica primeira letra da frase é a letra especificicada
+print(frutas.split(', '))#vai pegar a 'frutas' e retornar uma lista com cada tipo de fruta separada por virgula
+print(" ".join(lista_palavras))#alvaro mora no parque do lago
+frase_formatada = frase_zuada.title().strip().replace("Shitzu", "Alemão")
+print(frase_formatada)
