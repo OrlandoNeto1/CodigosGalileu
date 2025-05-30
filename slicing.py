75f0b59b0afaffc1a7447e64043172d0913569c9
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-05-30 15:42:40 -0300

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,15 @@
+#tecnica para seleção de um intervalo de listas, tuplas e dicionarios.
+tupla_nomes = ('alvaro',
+                'leticia',
+                'bruno',
+                'henrique',
+                'carlos')
+print(tupla_nomes[2:5])
+print(tupla_nomes[0:3])
+print(tupla_nomes[2:5])
+print(tupla_nomes[::2])
+print(tupla_nomes[::-1])
+
+
+#[1:3]= posição onde é selecionado
+
