bad394c9a72f386c702c41be533714b56fd92e66
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-07-14 11:26:25 -0300

    Add files via upload

--- loja_games.py
+++ loja_games.py
@@ -14,11 +14,34 @@ for jogo, info in loja_games.items():
     print(f'Preço : {info['preço']}')
     print(f'Estoque : {info['estoque']}')
     print('-'*40)
+while True: #roda infinito até digitar sair
+     
+    escolha_usuario = input ("Qual jogo você deseja:  (Caso queira encerrar digite 'Sair'):      ").strip().title()
+   
+    if escolha_usuario == 'Sair':
+        break 
+    if escolha_usuario in loja_games:
+        escolha_qtdade = int(input ("Quantas unidades você deseja?:  "))
+        if escolha_qtdade <= loja_games[escolha_usuario]['estoque']:
+            loja_games[escolha_usuario]['estoque']-= escolha_qtdade
+            if escolha_usuario in carrinho_de_compras:
+                carrinho_de_compras += escolha_qtdade
+            else:
+                carrinho_de_compras.setdefault(escolha_usuario, escolha_qtdade)
+        else:
+            print('Não temos esta quantidade, desculpe!')
+    else:
+        print('Não temos esse jogo disponivel, desculpe!')
 
-escolha_usuario = input ("Qual jogo você deseja:  ").strip().title()
 
-if escolha_usuario in loja_games:
-    escolha_qtdade = int(input ("Quantas unidades você deseja?:  "))
-    if escolha_qtdade <= loja_games[escolha_usuario]['estoque']:
-        loja_games[escolha_usuario]['estoque']-= escolha_qtdade
-        
+total = 0
+for jogo , qtd in carrinho_de_compras.items():
+    preço = loja_games[jogo]["preço"]
+    total += preço * qtd
+
+print (carrinho_de_compras)
+
+
+#print(carrinho_de_compras)
+#print('-'*40)
+#print(loja_games[escolha_usuario]['estoque'])
