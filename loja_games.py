f1b26b92226c1af7b0b4be03eec278d3bb72778e
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-07-14 10:17:42 -0300

    Add files via upload

--- loja_games.py
+++ loja_games.py
@@ -1,4 +1,4 @@
-loja_games_minecraft = {
+loja_games = {
 
 'Minecraft' : {'preço' : 59.90, 'estoque': 5},
 'Fifa23' :    {'preço' : 199.90, 'estoque' :3 },
@@ -6,11 +6,19 @@ loja_games_minecraft = {
 
 }
 
-print(loja_games_minecraft['Minecraft']['estoque'])
+carrinho_de_compras = {}
 
-for jogo, info in loja_games_minecraft.items:
+for jogo, info in loja_games.items():
 
     print(f'Jogo : {jogo}')
     print(f'Preço : {info['preço']}')
-    print(f'Preço{info['estoque']}')
+    print(f'Estoque : {info['estoque']}')
     print('-'*40)
+
+escolha_usuario = input ("Qual jogo você deseja:  ").strip().title()
+
+if escolha_usuario in loja_games:
+    escolha_qtdade = int(input ("Quantas unidades você deseja?:  "))
+    if escolha_qtdade <= loja_games[escolha_usuario]['estoque']:
+        loja_games[escolha_usuario]['estoque']-= escolha_qtdade
+        
