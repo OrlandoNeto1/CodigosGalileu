444cd85dcac8fd1017e0be68ecfa2f8d1ef5407b
OrlandoNeto1 <fornazeironeto@gmail.com>
2025-08-01 15:48:22 -0300

    Add files via upload

--- desafiopokemon.py
+++ desafiopokemon.py
@@ -1,26 +1,27 @@
-import random
-
 pokemons = {
     'Charmander': {'Ataque': 'Bola de Fogo', 'Dano': 35},
     'Squirtle': {'Ataque': 'Jato de água', 'Dano': 32},
     'Bulbassauro': {'Ataque': 'Chicote de planta', 'Dano': 30},
 }
 
-print ("Charmander': {'Ataque': 'Bola de Fogo', 'Dano': 35")
-print("Squirtle': {'Ataque': 'Jato de água', 'Dano': 32")
-print("Bulbassauro': {'Ataque': 'Chicote de planta', 'Dano': 30")
-print("_"*40)
-escolha1 = input("Escolha seu pokemon:  ")
 
-def pokemon1 (jogador1):
-    escolha1 = input("Escolha seu pokemon:  ")
-    if pokemon1 in pokemons:
-        print("Escolha do Jogador 1 Feita com sucesso!")
+def escolha_pokemon ():
+    print ("Faça Sua escolha: ".center(40 , '-'))
+    print ("1 = Charmander \n2 = Squirtle  \n3 = Bulbassauro")
+    escolha = int(input("Selecione 1, 2 ou 3:  "))
+    if escolha == 1:
+        escolha = 'Chamander'
+    elif escolha == 2:
+        escolha = 'Squirtle'
+    elif escolha == 3:
+        escolha = 'Bulbassauro'
+    else:
+        print("Desculpe não entendi! Digite 1, 2 ou 3")
+    return escolha
 
-def pokemon2 (jogdor2):
-    escolha2 = input("Escolha seu pokemon:  ")
-    if pokemon2 in pokemons:
-        print("Escolha do Jogador 2 Feita com sucesso!")
 
-pokemon1()
-pokemon2()
+jogador1 = escolha_pokemon()
+jogador2 = escolha_pokemon()
+
+print(jogador1)
+print(jogador2)
